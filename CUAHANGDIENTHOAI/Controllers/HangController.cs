c3f36d76cc2541942bc67f641d2e1c4a4201757a
xuanphuoc2509 <127526104+xuanphuoc2509@users.noreply.github.com>
2025-09-25 16:14:18 +0700

    controller don hang

---
+++ CUAHANGDIENTHOAI/Controllers/HangController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CUAHANGDIENTHOAI.Data;
+using CUAHANGDIENTHOAI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CUAHANGDIENTHOAI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangController : ControllerBase
+    {
+
+        public readonly ChdtContext _db;
+
+        public HangController(ChdtContext db) => _db = db;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllHang() =>
+            Ok(await _db.HANGs.AsNoTracking().ToListAsync());
+
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetHangById(int id)
+        {
+            var hang = await _db.HANGs.FindAsync(id);
+            return (hang is null) ? NotFound() : Ok(hang);
+        }
+        //    [HttpPost("{int:id}")]
+
+        //    public async Task<IActionResult> Create(int id)
+        //    {
+
+        //        var Hang = await _db.HANGs.FindAsync(id);
+
+
+        //        return NoContent();
+        //    }
+    }
+}
--- CUAHANGDIENTHOAI/Controllers/SanPhamController.cs
+++ CUAHANGDIENTHOAI/Controllers/SanPhamController.cs
@@ -15,44 +15,69 @@ namespace CUAHANGDIENTHOAI.Controllers
         public SanPhamController(ChdtContext context) => _context = context;
 
 
-        [HttpGet]
+
+
+        [HttpGet("lay-danh-sach-san-pham")]
         public async Task<IActionResult> getAll() =>
             Ok(await _context.SANPHAMs.AsNoTracking().ToListAsync());
 
+
+
         [HttpGet("{id:int}")]
-        public async Task<IActionResult> GetById(int id)
+        public async Task<IActionResult> getById(int id)
         {
             var sp = await _context.SANPHAMs.FindAsync(id);
             return sp is null ? NotFound() : Ok(sp);
         }
-        [HttpPost]
+
+        [HttpPost("them-san-pham")]
         public async Task<IActionResult> Create([FromBody] SANPHAM sp)
         {
             _context.SANPHAMs.Add(sp);
             await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetById), new { id = sp.SANPHAMID }, sp);
+            return CreatedAtAction(nameof(getById), new { id = sp.SANPHAMID }, sp);
         }
-        [HttpPut("{id:int}")]
+
+        //[HttpPut("sua-san-pham{id:int}")]
+        //public async Task<IActionResult> Update(int id, [FromBody] SANPHAM input)
+        //{
+        //    if (id != input.SANPHAMID) return BadRequest();
+        //    var sp = await _context.SANPHAMs.FindAsync(id);
+        //    if (sp is null) return NotFound();
+
+        //    sp.HANGID = input.HANGID;
+        //    sp.TENSANPHAM = input.TENSANPHAM;
+        //    sp.GIA = input.GIA;
+        //    sp.TONKHO = input.TONKHO;
+        //    sp.TRANGTHAI = input.TRANGTHAI;
+
+        //    await _context.SaveChangesAsync();
+
+
+        //    return NoContent();
+        //}
+
+        [HttpPut("sua-san-pham{id:int}")]
         public async Task<IActionResult> Update(int id, [FromBody] SANPHAM input)
         {
-            if (id != input.SANPHAMID) return BadRequest();
+           if(id != input.SANPHAMID ) return BadRequest();
             var sp = await _context.SANPHAMs.FindAsync(id);
             if (sp is null) return NotFound();
 
             sp.HANGID = input.HANGID;
             sp.TENSANPHAM = input.TENSANPHAM;
             sp.GIA = input.GIA;
+            sp.HANG = input.HANG;
             sp.TONKHO = input.TONKHO;
-            sp.TRANGTHAI = input.TRANGTHAI;
 
             await _context.SaveChangesAsync();
             return NoContent();
         }
-        [HttpDelete("delete {id:int}")]
+        [HttpDelete("xoa-san-pham{id:int}")]
         public async Task<IActionResult> Delete(int id)
         {
             var sp = await _context.SANPHAMs.FindAsync(id);
-            if (sp is null) return NotFound();
+
             _context.SANPHAMs.Remove(sp);
             await _context.SaveChangesAsync();
             return NoContent();
